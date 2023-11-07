88e1e51e5f778be850f7437d9a508994e1bdbbab
ReshadDev <reshadqarayev44@gmail.com>
2023-11-07 18:27:08 +0400

    new

---
+++ 05-travel-list/src/App.js
@@ -0,0 +1,14 @@
+export default function App(){
+
+}
+
+function Logo(){
+  return <h1>🌴Far Away</h1>
+}
+function Form(){
+  return <div className="add-form">
+    
+  </div>
+}
+function PackingList(){}
+function Stats(){}
